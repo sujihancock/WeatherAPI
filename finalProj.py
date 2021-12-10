17cdb441ff0c46d7a1f7259873ce3ea0b73796d3
Irvine <81997674+1rvine@users.noreply.github.com>
2021-12-09 18:57:52 -0800

    Finished HTML

--- finalProj.py
+++ finalProj.py
@@ -9,31 +9,36 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
 
+    temp = ''
+    feels_like =''
+    temp_max = ''
+    city=''
+    temp_min = ''
+    weather = ''
 
     if request.method == "POST":
-        city = request.form["city"]
 
+        city = request.form["city"]
 
         url = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&APPID=08eba528435fd4c005a6e7fd4d7668c1&units=imperial")
 
         data = url.json()
 
-        # weather_data = {
-        #     'temp' : data['main']['temp']
-        #     # 'feels_like' : data.main.feels_like,
-        #     # 'temp_min' : data.main.feels_like,
-        #     # 'temp_max' : data.main.temp_max
-        # }
-
-        # print(data['main']['temp'])
+        # temp = data['main']['temp']
+        # feels_like = data['main']['feels_like']
+        # temp_min = data['main']['temp_min']
+        # temp_max = data['main']['temp_max']
+        # return render_template('weather.html',city = city, temp = temp, feels_like = feels_like, temp_min = temp_min, temp_max = temp_max)
+        
         temp = data['main']['temp']
+        feels_like = data['main']['feels_like']
+        temp_max = data['main']['temp_max']
+        temp_min = data['main']['temp_min']
+        weather = data['weather'][0]['main']
 
 
 
-    return render_template('index.html', temp = temp)
+    return render_template('index.html', weather = weather,city=city, temp = temp, feels_like = feels_like, temp_max = temp_max, temp_min = temp_min)
 
 if __name__ == '__main__':
     app.run(debug = True)
-
-
-    # http://api.openweathermap.org/data/2.5/weather?q=Salinas&APPID=08eba528435fd4c005a6e7fd4d7668c1
